9eddda1eb009d06119b85bacb7dca395fabcfbf1
herman_kulik <hermankulik1995@gmail.com>
2022-08-11 21:54:27 +0200

    xml mvc

--- src/main/java/pl/her/springcourse/HelloController.java
+++ src/main/java/pl/her/springcourse/HelloController.java
@@ -1,2 +1,16 @@
-package pl.her.springcourse;public class HelloController {
+package pl.her.springcourse;
+
+import java.lang.reflect.Method;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+  @GetMapping("/hello-man")
+//  @RequestMapping(value = "/hello-man", method = RequestMethod.GET)
+  public String hello() {
+    return "hello-man"; //name of view
+  }
+
 }
